c3e037091364dad843ff60a5ea0923475d1db8e6
bruna23 <bruna.vf23@gmail.com>
2023-01-09 21:57:02 -0300

    exerc typr-2-exerc3

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import { users } from "./database"
-import { product } from "./database"
-import { purchase } from "./database"
+import { 
+    acessarProductId, 
+    acessarProductsName,
+    createPurchase,
+    getAllPurchasesFromUserId 
+} from "./database";
 
+console.log(acessarProductsName("teclado"));
+console.log(createPurchase("1", "1", 3, 15));
 //console.log(users, product, purchase)
 
 
-console.log("usuarios:\n", users, "produtos:\n", product, "compra realizada pelo cliente:\n", purchase )
+//console.log("usuarios:\n", users, "produtos:\n", product, "compra realizada pelo cliente:\n", purchase )
